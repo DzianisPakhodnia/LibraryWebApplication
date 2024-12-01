c1d1c86830a400c2c72bdec9e2796a9a0a893812
DzianisPakhodnia <denispokhodnia@gmail.com>
2024-12-02 01:33:51 +0300

    add entities

---
+++ LibraryWebApplication.Core/Entities/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWebApplication.Core.Entities
+{
+    internal class Book
+    {
+        public Guid Id { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+
+        public Author Author { get; set; }
+
+        public DateTime? BorrowedTime { get; set; }
+        public DateTime? ReturnTime { get; set; }
+    }
+}
