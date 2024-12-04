e9b69351872195b9e38f1bd568a9a22b07f1547a
DzianisPakhodnia <denispokhodnia@gmail.com>
2024-12-04 22:56:41 +0300

    add BookController

--- LibraryWebApplication.Core/Entities/User.cs
+++ LibraryWebApplication.Core/Entities/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,10 @@ namespace LibraryWebApplication.Core.Entities
 {
     internal class User
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
         public string RefreshToken { get; set; }
         public string AccessToken { get; set; }
 
